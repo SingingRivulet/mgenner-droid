c1243a34f04984d92e833a912d4c478ff9d9b650
cgoxopx <cgoxopx@qq.com>
2021-09-27 00:49:19 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # mgenner-droid
 
 #### 介绍
-安卓高性能midi编辑器  
+安卓高性能MIDI编辑器，编辑黑乐谱时依旧可以做到平均60fps，高能部分30+fps，并可以瞬间添加音符  
+
+##### 界面展示
 ![img](screenshot/Screenshot_20210926_115812_com.sinriv.midilib.jpg)  
 ![img](screenshot/Screenshot_20210926_123851_com.sinriv.midilib.jpg)  
 ![img](screenshot/Screenshot_20210926_205158_com.sinriv.mgenner.jpg)  
@@ -12,7 +14,7 @@
 
 #### 编译方法
 
-直接使用Android Studio编译  
+建议使用Android Studio编译，ndk需要支持C++20  
 
 
 #### 参与贡献
--- app/src/main/java/com/sinriv/mgenner/MainActivity.java
+++ app/src/main/java/com/sinriv/mgenner/MainActivity.java
@@ -5,13 +5,5 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this,com.sinriv.midilib.mgenner.midiEditor.class);
-        startActivity(intent);
-    }
-
+public class MainActivity extends com.sinriv.midilib.mgenner.midiEditor {
 }
